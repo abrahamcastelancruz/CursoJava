3102267001228e44a235d80a378958449af7409e
Abraham Castelan Cruz <64657935+abrahamcastelancruz@users.noreply.github.com>
2020-10-20 23:56:21 -0500

    Curso de Java Desde 0 Parte 1 20/10/2020

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author cobas
+ *
+ */
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// Este es un comentario
+		/* Este es un comentario*/
+		System.out.println("Hola Mundo");
+		// \n -> nuevo salto de linea
+		System.out.println("Nueva impresión");
+		System.out.println("Después de println");
+		//System.out
+		System.out.println("Abraham\nCastelan Cruz\n22");
+		System.out.println("----------------");
+		System.out.print("Abraham\n");
+		
+	}
+
+}
---
+++ Variables/src/Variables.java
@@ -0,0 +1,41 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// 8 tipos de datos primitivos
+		
+		//Enteros
+			byte n1 = 10; //'11111111' -128 a 127
+			System.out.println("La variable n1 es : "+n1);
+
+			short n2 = 30; //Miles
+			//System.out.println(n2);
+			System.out.println("La variable n2 es : "+n2);
+
+			int edad = 22; //Miles de millones
+			//System.out.println(edad);
+			System.out.println("Mi edad es : "+edad);
+
+			long b3 = 1231231; //Miles de billones
+			//System.out.println(b3);
+			System.out.println("La variable b3 es : "+b3);
+		
+		//Punto decimal o Reales
+			float area = 16; // 32 bits
+			System.out.println("La variable area es : "+area);
+			
+			double perimetro = 28; // 64 bits
+			System.out.println("La variable perimetro es : "+perimetro);
+			
+		//Char
+			char letra = 98;
+			System.out.println(letra);
+		
+		//Boolean 
+			boolean x = true;
+			boolean y = false;
+			System.out.println(x);
+		
+	}
+
+}
